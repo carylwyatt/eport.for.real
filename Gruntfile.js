0ab168e73009b89ca1628815881a4cd274b45223
carylwyatt <caryl.e.wyatt@gmail.com>
2015-02-01 23:19:10 -0500

    grunt update

---
+++ Gruntfile.js
@@ -0,0 +1,215 @@
+module.exports = function(grunt) {
+ 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            dist: {
+                src: 'readme.txt',
+                dest: 'README.md'
+            }
+        },
+        /*
+        curl: {
+            'google-fonts-source': {
+                src: 'https://www.googleapis.com/webfonts/v1/webfonts?key=*******',
+                dest: 'wp-content/themes/eport/library/vendor/google-fonts-source.json'
+            }
+        },
+        makepot: {
+            target: {
+                options: {
+                    include: [
+                        'path/to/some/file.php'
+                    ],
+                    type: 'wp-plugin' // or `wp-theme`
+                }
+            }
+        },
+        */
+        jshint: {
+            files: [
+                'wp-content/themes/eport/library/js/filename.js',
+                'wp-content/themes/eport/library/js/**/*.js'
+            ],
+            options: {
+                expr: true,
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        /*
+        phpdocumentor: {
+            dist: {
+                options: {
+                    ignore: 'node_modules'
+                }
+            }
+        },
+        */
+        sass: {
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> style.min.css <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    style: 'compressed'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'wp-content/themes/eport/library/scss',
+                    src: [
+                        '*.scss'
+                    ],
+                    dest: 'wp-content/themes/eport/library/css',
+                    ext: '.min.css'
+                }]
+            },
+            dev: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> style.css <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    style: 'expanded'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'wp-content/themes/eport/library/scss',
+                    src: [
+                        '*.scss'
+                    ],
+                    dest: 'wp-content/themes/eport/library/css',
+                    ext: '.css'
+                }]
+            }
+        },
+        /*
+        libsass: {
+            options: {
+              loadPath: ['wp-content/themes/eport/bower_components/foundation/scss']
+            },
+            files: [
+                {
+                    expand: true,
+                    cwd: 'wp-content/themes/eport/library/scss',
+                    src: [
+                        '*.scss'
+                    ],
+                    dest: 'wp-content/themes/eport/library/css',
+                    ext: '.css'
+                }
+            ],
+        },
+        */
+        /*
+        compass: {
+            dist: {
+                options: {
+                    outputStyle: 'compressed',
+                    httpPath: '/',
+                    
+                    watch: true,
+                    sassDir: 'wp-content/themes/eport/library/scss',
+                    cssDir: ['wp-content/themes/eport/library/css/'],
+                    imagesDir: 'wp-content/themes/eport/library/images',
+                    javascriptsDir: 'wp-content/themes/eport/library/js'    
+                }
+            },
+            dev: {
+                options: {
+                    outputStyle: 'expanded',//'compressed',
+                    httpPath: '/',
+                    watch: true,
+                    sassDir: 'wp-content/themes/eport/library/scss',
+                    cssDir: ['wp-content/themes/eport/library/css/'],
+                    imagesDir: 'wp-content/themes/eport/library/images',
+                    javascriptsDir: 'wp-content/themes/eport/library/js'    
+                }
+            }
+        },
+        */
+
+        uglify: {
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> scripts.min.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    report: 'gzip'
+                },
+                files: {
+                    'wp-content/themes/eport/library/js/scripts.min.js' : [
+                        'wp-content/themes/eport/bower_components/foundation/js/vendor/jquery.js',
+                        'wp-content/themes/eport/bower_components/foundation/js/vendor/modernizr.js',
+                        'wp-content/themes/eport/bower_components/foundation.min.js',
+                        //'wp-content/themes/eport/library/js/**/*.js'
+                    ]
+                }
+            },
+            dev: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> scripts.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    beautify: true,
+                    compress: false,
+                    mangle: false
+                },
+                files: {
+                    'wp-content/themes/eport/library/js/scripts.js' : [
+                        'wp-content/themes/eport/bower_components/foundation/js/vendor/jquery.js',
+                        'wp-content/themes/eport/bower_components/foundation/js/vendor/modernizr.js',
+                        'wp-content/themes/eport/bower_components/foundation.min.js',
+                        //'wp-content/themes/eport/library/js/**/*.js'
+                    ]
+                }
+            }
+        },
+
+        watch: {
+            //Livereload
+            scripts: {
+                files: 'wp-content/themes/eport/**/*.js',
+                tasks: ['uglify:dev', 'uglify:dist'],
+            },
+            sass: {
+                files: 'wp-content/themes/eport/library/scss/**/*.scss',
+                tasks: ['sass:dev', 'sass:dist'],
+            },
+            options: {
+                livereload: 35729
+            }
+        }
+    });
+ 
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    //grunt.loadNpmTasks('grunt-libsass');
+    //grunt.loadNpmTasks('grunt-curl');
+    //grunt.loadNpmTasks('grunt-phpdocumentor');
+    //grunt.loadNpmTasks('grunt-wp-i18n');
+ 
+    grunt.registerTask('default', [
+       // 'jshint',
+        'uglify:dev',
+        'uglify:dist',
+        'sass:dev',
+        'sass:dist',
+        'watch',
+        //'libsass',
+        //'compass:dev',
+        //'compass:dist',
+        //'makepot',
+        'copy'
+    ]);
+    /*
+    grunt.registerTask('docs', [
+        'phpdocumentor:dist'
+    ]);
+    */
+    /*
+    grunt.registerTask('googlefonts', [
+        'curl:google-fonts-source'
+    ]);
+    */
+ 
+};
---
+++ wp-content/themes/eport/header.php
@@ -0,0 +1,73 @@
+<!doctype html>
+
+  <html class="no-js"  <?php language_attributes(); ?>>
+
+	<head>
+		<meta charset="utf-8">
+		
+		<!-- Force IE to use the latest rendering engine available -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<title><?php wp_title(''); ?></title>
+
+		<!-- Mobile Neta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+		<!-- Icons & Favicons -->
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png" rel="apple-touch-icon" />
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+		 <meta name="theme-color" content="#121212">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<?php wp_head(); ?>
+
+		<!-- Drop Google Analytics here -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-32501186-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+		<!-- end analytics -->
+
+	</head>
+
+	<body <?php body_class(); ?>>
+		<div class="off-canvas-wrap" data-offcanvas>
+			<div class="inner-wrap">
+				<div id="container">
+					<header class="header" role="banner">
+							
+						 <!-- This navs will be applied to the topbar, above all content 
+							  To see additional nav styles, visit the /partials directory -->
+						 <?php get_template_part( 'partials/nav', 'top-offcanvas' ); ?>
+								 
+						<div id="inner-header" class="row">
+							<div class="large-12 medium-12 columns">
+								<h1>
+									<a href="<?php echo home_url(); ?>" rel="nofollow">
+										<?php bloginfo('name'); ?>
+									</a>
+									<small>
+										<?php  bloginfo('description'); ?>
+									</small>
+								</h1>
+							</div>
+							
+							 <!-- This navs will be applied to the main, under the logo 
+								  To see additional nav styles, visit the /partials directory -->
+								  
+							 <?php // get_template_part( 'partials/nav', 'main-offcanvas' ); ?>
+	
+						</div> <!-- end #inner-header -->
+					</header> <!-- end .header -->
